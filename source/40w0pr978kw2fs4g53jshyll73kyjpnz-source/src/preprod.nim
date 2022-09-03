ef1eb7ab27f3ccdc9bc00e4ac6ba2fbd1018f338
hugosenari <hugosenari@gmail.com>
2022-09-03 19:51:01 -0300

    feat(nimpkgs): update lock

--- source/40w0pr978kw2fs4g53jshyll73kyjpnz-source/src/preprod.nim
+++
@@ -1,7 +0,0 @@
-# preprod by Javier Santo Domingo (j-a-s-d@coderesearchlabs.com)
-
-import xam
-
-reexport(preprod / exports, exports)
-
-let PREPROD_VERSION*: SemanticVersion = newSemanticVersion(1, 1, 1)
